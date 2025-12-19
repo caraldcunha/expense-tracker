30ad3b1d5e750a67777883d60db15444e9754a43
AshwithaMDsouza <ashwithamdsouza26@gmail.com>
2025-12-19 15:12:19 +0530

    Update heap.py

--- app/dsa/heap.py
+++ app/dsa/heap.py
@@ -1,1 +1,15 @@
+import heapq
 
+class WeeklyWrapped:
+    def __init__(self):
+        self.category_totals = {}
+
+    def add_expense(self, category, amount):
+        if category not in self.category_totals:
+            self.category_totals[category] = 0
+        self.category_totals[category] += amount
+
+    def get_top_categories(self, n=3):
+        heap = [(-amount, cat) for cat, amount in self.category_totals.items()]
+        heapq.heapify(heap)
+        return [(cat, -amount) for amount, cat in heapq.nsmallest(n, heap)]
