1ff37069a31f3b618268223c6751c7aa5b3066b9
AshwithaMDsouza <ashwithamdsouza26@gmail.com>
2025-12-19 15:11:30 +0530

    Update hash_map.py

--- app/dsa/hash_map.py
+++ app/dsa/hash_map.py
@@ -1,1 +1,15 @@
+class CategoryMap:
+    def __init__(self):
+        self.map = {}
+
+    def add_expense(self, category, amount):
+        if category not in self.map:
+            self.map[category] = 0
+        self.map[category] += amount
+
+    def get_total_by_category(self, category):
+        return self.map.get(category, 0)
+
+    def get_all_categories(self):
+        return self.map
 
