d2116d32ed6982769eb04d4a7c29b317614b8c9e
caraldcunha <caraldcunha475@gmail.com>
2025-12-20 01:17:31 +0530

    Update heap.py

--- app/dsa/heap.py
+++ app/dsa/heap.py
@@ -13,21 +13,6 @@ class WeeklyWrapped:
         heap = [(-amount, cat) for cat, amount in self.category_totals.items()]
         heapq.heapify(heap)
         return [(cat, -amount) for amount, cat in heapq.nsmallest(n, heap)]
-        import heapq
-
-class WeeklyWrapped:
-    def __init__(self):
-        self.category_totals = {}
-
-    def add_expense(self, category, amount):
-        if category not in self.category_totals:
-            self.category_totals[category] = 0
-        self.category_totals[category] += amount
-
-    def get_top_categories(self, n=3):
-        heap = [(-amount, cat) for cat, amount in self.category_totals.items()]
-        heapq.heapify(heap)
-        return [(cat, -amount) for amount, cat in heapq.nsmallest(n, heap)]
 
 # ---- Generic MinHeap to satisfy tests ----
 class MinHeap:
@@ -49,4 +34,3 @@ class MinHeap:
 
     def is_empty(self):
         return len(self._heap) == 0
-
