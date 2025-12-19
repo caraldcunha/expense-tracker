d210fc007fceb3d8490b815873f200bc288da921
caraldcunha <caraldcunha475@gmail.com>
2025-12-19 23:56:24 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,20 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-db = SQLAlchemy()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/expense_tracker.sqlite'
+
+db = SQLAlchemy(app)
+
 login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your-secret-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/expense_tracker.sqlite'
-
-    db.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_view = 'login'
-
-    with app.app_context():
-        from . import models, routes  # Make sure these files exist
-        db.create_all()
-
-    return app
+from app import routes, models
