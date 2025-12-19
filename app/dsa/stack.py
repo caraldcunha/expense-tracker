55220443cc73b1c60d0f122a3bbd1af42ed0f6ac
AshwithaMDsouza <ashwithamdsouza26@gmail.com>
2025-12-19 15:19:43 +0530

    Update stack.py

--- app/dsa/stack.py
+++ app/dsa/stack.py
@@ -1,1 +1,15 @@
+class UndoStack:
+    def __init__(self):
+        self.stack = []
+
+    def push_action(self, action_type, data):
+        self.stack.append((action_type, data))  # e.g., ("add", transaction_data)
+
+    def pop_action(self):
+        if self.stack:
+            return self.stack.pop()
+        return None
+
+    def is_empty(self):
+        return len(self.stack) == 0
 
