77de0f32065acf0f0f0bb9dbcd2aae18df90657c
AshwithaMDsouza <ashwithamdsouza26@gmail.com>
2025-12-19 15:17:15 +0530

    Update queue.py

--- app/dsa/queue.py
+++ app/dsa/queue.py
@@ -1,1 +1,18 @@
+class NotificationQueue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, message):
+        self.queue.append(message)
+
+    def dequeue(self):
+        if self.queue:
+            return self.queue.pop(0)
+        return None
+
+    def peek(self):
+        return self.queue[0] if self.queue else None
+
+    def is_empty(self):
+        return len(self.queue) == 0
 
