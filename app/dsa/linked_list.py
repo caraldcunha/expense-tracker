0dccf1fd46b847a9f14f494827402dc4ccbbae7b
caraldcunha <caraldcunha475@gmail.com>
2025-12-20 01:18:04 +0530

    Update linked_list.py

--- app/dsa/linked_list.py
+++ app/dsa/linked_list.py
@@ -1,6 +1,6 @@
 class TransactionNode:
     def __init__(self, data):
-        self.data = data  # dict with keys: date, amount, category, description
+        self.data = data
         self.next = None
 
 class TransactionList:
@@ -21,22 +21,56 @@ class TransactionList:
         curr = self.head
         prev = None
         while curr:
-            if curr.data['description'] == description:
-                if prev:
-                    prev.next = curr.next
-                else:
-                    self.head = curr.next
+            if curr.data.get('description') == description:
+                if prev: prev.next = curr.next
+                else: self.head = curr.next
                 return True
             prev = curr
             curr = curr.next
         return False
 
     def list_transactions(self):
-        result = []
-        curr = self.head
+        result, curr = [], self.head
         while curr:
             result.append(curr.data)
             curr = curr.next
         return result
 
+# ---- Generic LinkedList to satisfy tests ----
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+        else:
+            curr = self.head
+            while curr.next:
+                curr = curr.next
+            curr.next = new_node
+
+    def delete(self, value):
+        curr = self.head
+        prev = None
+        while curr:
+            if curr.value == value:
+                if prev: prev.next = curr.next
+                else: self.head = curr.next
+                return True
+            prev = curr
+            curr = curr.next
+        return False
+
+    def traverse(self):
+        out, curr = [], self.head
+        while curr:
+            out.append(curr.value)
+            curr = curr.next
+        return out
