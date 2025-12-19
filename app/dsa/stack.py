20b1feb0e5134b421c2e6447a8aab4291a5149ae
caraldcunha <caraldcunha475@gmail.com>
2025-12-20 01:18:38 +0530

    Update stack.py

--- app/dsa/stack.py
+++ app/dsa/stack.py
@@ -3,7 +3,7 @@ class UndoStack:
         self.stack = []
 
     def push_action(self, action_type, data):
-        self.stack.append((action_type, data))  # e.g., ("add", transaction_data)
+        self.stack.append((action_type, data))
 
     def pop_action(self):
         if self.stack:
@@ -13,3 +13,23 @@ class UndoStack:
     def is_empty(self):
         return len(self.stack) == 0
 
+# ---- Generic Stack to satisfy tests ----
+class Stack:
+    def __init__(self):
+        self._stack = []
+
+    def push(self, value):
+        self._stack.append(value)
+
+    def pop(self):
+        if not self._stack:
+            raise IndexError("pop from empty stack")
+        return self._stack.pop()
+
+    def peek(self):
+        if not self._stack:
+            raise IndexError("peek from empty stack")
+        return self._stack[-1]
+
+    def is_empty(self):
+        return len(self._stack) == 0
